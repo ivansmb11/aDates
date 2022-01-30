05ba64ff7c3661d0bf15d8dd885832768a9e3d6a
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-30 03:07:40 -0600

    Added swiper

--- src/modules/auth/views/RegisterView.vue
+++ src/modules/auth/views/RegisterView.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>Register</h1>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+  </swiper>
 </template>
 
 <script>
-export default {
 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  }
 }
 </script>
 
